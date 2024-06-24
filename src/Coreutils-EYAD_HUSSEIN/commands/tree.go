440a9f63ee01151532749cf0336c8cc6ca453378
eyad-hussein <eyad.hussein@ejust.edu.eg>
2024-06-24 11:23:32 +0300

    Implemented tree command

--- src/Coreutils-EYAD_HUSSEIN/commands/tree.go
+++ src/Coreutils-EYAD_HUSSEIN/commands/tree.go
@@ -1,1 +1,106 @@
 package commands
+
+import (
+	"Coreutils-EYAD_HUSSEIN/models"
+	"Coreutils-EYAD_HUSSEIN/utils"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+func Tree(flags []string) {
+	command := models.CommandsMap["tree"]
+	
+	if !utils.ValidateFlags(flags, command.Flags) {
+		os.Exit(1)
+	}
+
+	maxDepth := -1
+
+	if ok := utils.ContainsFlag(flags, "-L"); ok {
+		idx := utils.GetIndexOfArg("-L")
+		tempMaxDepth, err := strconv.Atoi(os.Args[idx+1])
+		if err != nil {
+			fmt.Println("Invalid depth value")
+			os.Exit(1)
+		}
+		maxDepth = tempMaxDepth
+	}
+
+	if maxDepth == -1{
+		printDirectory(os.Args[2], 0, maxDepth)
+	} else {
+		for _, arg := range os.Args[2:] {
+			if arg == "-L" || arg == strconv.Itoa(maxDepth) {
+				continue
+			}
+			printDirectory(arg, 0, maxDepth)
+		}
+	}
+}
+
+func printListing(entry string, depth int, maxDepth int) {
+	if maxDepth != -1 && depth > maxDepth {
+		return
+	}
+
+	indent := strings.Repeat("|   ", depth)
+
+	if depth == 0 {
+		fmt.Println(entry)
+	} else {
+		entrySplit := strings.Split(entry, "/")
+		var entryName string
+			
+		if entrySplit[len(entrySplit)-1] == "" {
+			entryName = entrySplit[len(entrySplit)-2]
+		} else {
+			entryName = entrySplit[len(entrySplit)-1]
+		}
+		
+		fmt.Printf("%s|-- %s\n", indent, entryName)
+	}
+}
+
+func printDirectory(path string, depth, maxDepth int) {
+	if  maxDepth != -1 && depth > maxDepth {
+		return
+	}
+
+	if ent := strings.Split(path, "/")[len(strings.Split(path, "/"))-1]; depth != 0 && ent != "" && ent[0] == '.' {
+		return
+	}
+
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		fmt.Printf("error reading %s: %s\n", path, err.Error())
+		return
+	}
+
+
+	printListing(path, depth, maxDepth)
+
+	for _, entry := range entries {
+		entryPath := filepath.Join(path, entry.Name())
+		fileInfo, err := os.Lstat(entryPath)
+		if err != nil {
+			fmt.Printf("error reading %s: %s\n", entryPath, err.Error())
+			continue
+		}
+
+		if fileInfo.Mode()&os.ModeSymlink != 0 {
+			fullPath, err := os.Readlink(entryPath)
+			if err != nil {
+				fmt.Printf("error reading link: %s\n", err.Error())
+			} else {
+				printListing(entry.Name()+" -> "+fullPath, depth+1, maxDepth)
+			}
+		} else if fileInfo.IsDir() {
+			printDirectory(entryPath, depth+1, maxDepth)
+		} else {
+			printListing(entry.Name(), depth+1, maxDepth)
+		}
+	}
+}
